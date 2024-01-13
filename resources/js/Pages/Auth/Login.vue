d0f60ee3ca54f984643b730d4c575368423bd610
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-01-14 00:15:42 +0300

    Add register link in Login page
    - A dds a new link to the register page in the Login page.
    - Users can now easily navigate to the register page if they
      don't have an existing account.

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -87,6 +87,13 @@ const submit = () => {
                     Log in
                 </PrimaryButton>
             </div>
+            <!-- New link for the register page -->
+            <Link
+                :href="route('register')"
+                class="ms-4 underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+                Don't have an account? Register here.
+            </Link>
         </form>
     </GuestLayout>
 </template>
